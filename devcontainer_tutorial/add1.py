57f8f8e075633385c15b961b68759155faf484df
Thomas Camminady <0milieux_member@icloud.com>
2022-10-21 07:13:08 +0000

    updates.

--- devcontainer_tutorial/add1.py
+++ devcontainer_tutorial/add1.py
@@ -1,14 +1,3 @@
 def add1(x: int) -> int:
-    """this is a test
 
-    Parameters
-    ----------
-    x : int
-        asd
-
-    Returns
-    -------
-    int
-        asd
-    """
     return x + 1
